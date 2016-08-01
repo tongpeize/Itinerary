6b48dd6bac770412da7186d6596887baedb6952c
tongpeize <TPZ@EKUAIBAO.COM>
2016-08-01 20:55:21 +0800

    制作UI

---
+++ Itinerary/RouteTableViewController.swift
@@ -0,0 +1,20 @@
+//
+//  RouteTableViewController.swift
+//  Itinerary
+//
+//  Created by 佟佩泽 on 16/8/1.
+//  Copyright © 2016年 佟佩泽. All rights reserved.
+//
+
+import UIKit
+
+class RouteTableViewController: UITableViewController {
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+}
---
+++ Itinerary/TableViewCell.swift
@@ -0,0 +1,56 @@
+//
+//  TableViewCell.swift
+//  Itinerary
+//
+//  Created by 佟佩泽 on 16/8/1.
+//  Copyright © 2016年 佟佩泽. All rights reserved.
+//
+
+import UIKit
+
+class CityCell: UITableViewCell {
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //TODO
+    }
+    
+}
+
+
+class FlightCell: UITableViewCell {
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //TODO
+    }
+    
+}
+
+
+class HotelCell: UITableViewCell {
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //TODO
+    }
+    
+}
+
+
+class AddCell: UITableViewCell {
+    
+    
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        //TODO
+    }
+    
+}
